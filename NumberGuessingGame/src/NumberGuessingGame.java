0c376ed59ac5b3818b99520146c409e7dc27b9ba
Ada <67380485+AdaN6@users.noreply.github.com>
2024-07-05 23:06:46 +0200

    added number of guesses

--- NumberGuessingGame/src/NumberGuessingGame.java
+++ NumberGuessingGame/src/NumberGuessingGame.java
@@ -11,14 +11,20 @@ public class NumberGuessingGame {
 		int randomNumber = rand.nextInt(100) + 1;
 		// System.out.println("Random number is " + randomNumber);
 		
+		// Score
+		int tryCount= 0;
+		
 		// while loop is for after the wrong guess and guess again
 		while(true) {
 			System.out.println("Enter your guess (1-100):");
 			
 			int playerGuess = scanner.nextInt();
+			tryCount++;
 			
 			if (playerGuess == randomNumber) {
 				System.out.println("Correct! Smart guess!");
+				// message of the score
+				System.out.println( tryCount + " guesses");
 				// If the user guess the right answer, the loop wont happen.
 				break;
 			} 
@@ -29,6 +35,8 @@ public class NumberGuessingGame {
 				System.out.println("Nope! The number is lower. Guess again.");
 			}
 		}
+		
+		scanner.close();
 	}
 
 }
